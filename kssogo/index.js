ce0aa773bad14d18bf51d6e06e2dc5bae8603061
lenovomi0403 <lenovomi0403@gmail.com>
2020-05-26 19:18:23 +0800

    Upload v3
    fix

--- kssogo/index.js
+++ kssogo/index.js
@@ -3,3 +3,22 @@
 //     $("#sidebar").toggleClass('active');
 //     });
 // });
+
+var gtt = document.querySelector('.GoToTop')
+window.onscroll = function(){
+
+    var nowScrollTop_safari = document.body.scrollTop
+    var nowScrollTop_google = document.documentElement.scrollTop
+    
+    if(nowScrollTop_safari > 100  || nowScrollTop_google >100){
+        gtt.classList.add('active')
+    }else{
+        gtt.classList.remove('active')
+    }
+}
+
+
+gtt.onclick = function(){
+    document.documentElement.scrollTop = 0
+    document.body.scrollTop = 0
+}
